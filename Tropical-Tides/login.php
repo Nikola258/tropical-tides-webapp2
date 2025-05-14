fcb0d3cc113b39df46447da04bb17810b3bf395c
Nikola258 <nikolic07nikola@gmail.com>
2025-05-14 11:41:52 +0200

    Commit 3 Author: Nikola Datum: 14-05-2025 11:438
    Login page aangemaakt & login page video background toegevoegd. Kleine front-end aanpassing in de about page.
    
    - Ik heb een login.php file aangemaakt.
    - Ik heb een video background toegevoegd in de login.php.
    - Ik heb bij de about page de hero content een bottom border gegeven en een shadow.
    - Video source van de website www.pexels.com.

--- Tropical-Tides/login.php
+++ Tropical-Tides/login.php
@@ -5,10 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
+    <title>Login Page</title>
 </head>
 <body>
-    <div>Hello world!</div>
+
+
+<!-- Video Background -->
+<video autoplay muted loop>
+    <source src="assets/img/login-background.mp4" type="video/mp4">
+    Your browser does not support the video tag.
+</video>
+
 
 </body>
 </html>
