286e0f551f2ba9cacc4e14f7d30b227ff3dd4a9d
Nikola258 <nikolic07nikola@gmail.com>
2025-05-26 13:14:55 +0200

    Feature | Admin dashboard & Admin navbar. Author: Nikola Datum: 26-05-2025 13:14
    - De files die ik heb aangemaakt:
       ~ admin dashboard page
       ~ messages page
       ~ customers page
       ~ destinations page
       ~ bookings page
       ~ logout
       ~ admin.css file
       ~ dashboard sidebar in include directory

---
+++ Tropical-Tides/admin-dashboard-views/customers.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/admin.css">
+</head>
+<body>
+<?php include "../include/dashboard-sidebar.php"; ?>
+
+    <div class="content">
+        <h1>Welcome to the Customers Page.</h1>
+        <p>Here you can review and manage incoming messages from customers to provide timely support and communication.</p>
+    </div>
+
+</body>
+</html>
---
+++ Tropical-Tides/admin-dashboard.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php include "include/dashboard-sidebar.php"; ?>
+
+    <div class="content">
+        <h1>Welcome to the Admin Dashboard.</h1>
+        <p>This is your main content area.</p>
+    </div>
+
+</body>
+</html>
